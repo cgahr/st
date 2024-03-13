14926bb3ba140ffb7e0fb565ad74ff71605dfd8f
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2024-03-13 16:00:55 +0100

    fix color assignment

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "UbuntuMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -116,11 +116,10 @@ static const char *colorname[] = {
 
 	[255] = 0,
 
-	/* more colors can be added after 255 to use with DefaultXX */
 	[256]=FOREGROUND,
 	[257]=BACKGROUND,
-	[258]=LIGHTERGREY, /* default foreground colour */
-	[259]=DARKGREY, /* default background colour */
+	[258]=LIGHTERGREY,
+	[259]=DARKGREY,
 };
 
 
@@ -128,10 +127,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 259;
 
 /*
  * Default shape of cursor
